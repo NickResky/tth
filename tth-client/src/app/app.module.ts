806a473fba70e5965531f4bf5808654e7aae39f8
Mogwayi <Yasmin.Hollenbenders@gmx.de>
2017-08-23 12:36:39 +0200

    :sparkles: :lipstick: Introduce Parallax on Main Page

--- tth-client/src/app/app.module.ts
+++ tth-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
