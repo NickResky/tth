a0bdbb3c43c8a5cd37606c1c1e0d8e2d66070d0d
Mogwayi <Yasmin.Hollenbenders@gmx.de>
2017-09-07 21:14:17 +0200

    :hankey: First try to make main page dynamic

--- tth-client/src/app/app.module.ts
+++ tth-client/src/app/app.module.ts
@@ -1,20 +1,25 @@
+import { SiteService } from './main-page/site.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SiteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
