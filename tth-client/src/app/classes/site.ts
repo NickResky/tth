3682ec930b6a712ade99bcffb1fd1320bdefe418
Mogwayi <Yasmin.Hollenbenders@gmx.de>
2017-09-07 20:51:57 +0200

    Create mock data for main page

---
+++ tth-client/src/app/classes/site.ts
@@ -0,0 +1,5 @@
+export class Site {
+    title: string;
+    description: string;
+    // pictureURL: string;
+}
---
+++ tth-client/src/resources/mock-data/site-data.ts
@@ -0,0 +1,28 @@
+import { Site } from '../../app/classes/site';
+
+export var SITES: Site[] = [
+    {
+        'title': 'Aktuelles',
+        'description': 'Hier findet ihr Neuigkeiten rund um das Tanztheater.'
+    },
+    {
+        'title': 'Kurse',
+        'description': 'Über die gesamte Kursübersicht, Stundenpläne und die aktuellen Kurspreise könnt ihr euch hier informieren.'
+    },
+    {
+        'title': 'Bühne',
+        'description': 'Wir inszenieren Kindermusicals, Jugendprojekte und Choreographenabende in Theatern und im Forum Ludwigsburg. Hier könnt ihr die besten Bilder vergangene Auftritte anschauen.'
+    },
+    {
+        'title': 'Team',
+        'description': ''
+    },
+    {
+        'title': 'Standorte',
+        'description': 'So findet ihr zu uns.'
+    },
+    {
+        'title': 'Kontakt',
+        'description': 'Wir freuen uns über Begegnungen mit tanzbegeisterten Menschen, die zum Probetraining vorbeischauen oder sich von unseren Auftritten bezaubern lassen.'
+    }
+]
