b5e95d81d6d4d603f9cab2def90a419b3a865d86
Mogwayi <Yasmin.Hollenbenders@gmx.de>
2017-09-08 16:48:40 +0200

    :sparkles: Introduce "Aktuelles" page

--- tth-client/src/app/app.module.ts
+++ tth-client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CurrentComponent } from './sites/current/current.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CurrentComponent
   ],
   imports: [
     BrowserModule
