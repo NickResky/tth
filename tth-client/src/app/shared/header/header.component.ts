912ab48f9ba099a6199c1c87715acf7bd3d1a860
Mogwayi <Yasmin.Hollenbenders@gmx.de>
2017-09-09 21:28:56 +0200

    :art: Outsource html-code

--- tth-client/src/app/shared/header/header.component.ts
+++ tth-client/src/app/shared/header/header.component.ts
@@ -2,17 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-  <div class="navbar">
-    <div routerLink="/" class="logo"></div>
-    <a href="#contact">Kontakt</a>
-    <a href="#locations">Standorte</a>
-    <a href="#team">Team</a>
-    <a href="#courses">Kurse</a>
-    <a href="#stage">Bühne</a>
-    <a routerLink="/aktuelles">Aktuelles</a>
-  </div>
-  `,
+  templateUrl: 'header.component.html',
   styleUrls: ['header.component.scss']
 })
 export class HeaderComponent implements OnInit {
