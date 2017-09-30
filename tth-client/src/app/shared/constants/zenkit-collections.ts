57c1d83a70e6a370d72d04d72bfa49270afbb389
NickResky <nick.resky@gmail.com>
2017-09-30 16:07:46 -0400

    :sparkles: Add constants for zenkit imports

---
+++ tth-client/src/app/shared/constants/zenkit-collections.ts
@@ -0,0 +1,9 @@
+import { RequiredElements } from './required-elements';
+
+export const ZenkitCollections  = {
+    current: {
+        shortId: 'SyNHkctub',
+        // LOCAL shortId: 'BJFD1TY9W',
+        requiredElements: RequiredElements.current
+    },
+};
